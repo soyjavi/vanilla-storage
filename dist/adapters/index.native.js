74b438906c4c2858a5207750c143b194c6ddc67f
JΛVI <hello@soyjavi.com>
2021-07-10 09:11:35 +0700

    📦 Release 0.3.14

---
+++ dist/adapters/index.native.js
@@ -0,0 +1,31 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "AsyncJsonAdapter", {
+  enumerable: true,
+  get: function get() {
+    return _memory.MemoryAdapter;
+  }
+});
+Object.defineProperty(exports, "JsonAdapter", {
+  enumerable: true,
+  get: function get() {
+    return _memory.MemoryAdapter;
+  }
+});
+Object.defineProperty(exports, "MemoryAdapter", {
+  enumerable: true,
+  get: function get() {
+    return _memory.MemoryAdapter;
+  }
+});
+Object.defineProperty(exports, "StorageAdapter", {
+  enumerable: true,
+  get: function get() {
+    return _memory.MemoryAdapter;
+  }
+});
+
+var _memory = require("./memory");
---
+++ src/adapters/index.native.js
@@ -0,0 +1,8 @@
+import { MemoryAdapter } from './memory';
+
+export {
+  MemoryAdapter as AsyncJsonAdapter,
+  MemoryAdapter as JsonAdapter,
+  MemoryAdapter,
+  MemoryAdapter as StorageAdapter,
+};
